01d35eb86652b3707cbd3246a5c6e472481b2bbc
Marcos Alves Faino <1812293@ifro.local>
2025-07-02 22:21:05 -0400

    tentativa de integração

--- drag.js
+++ drag.js
@@ -1,18 +1,36 @@
 window.onload = function() {
     // find the element that you want to drag.
     var box = document.getElementById('box');
-    
+    var dest = document.getElementById('destino');
     /* listen to the touchMove event,
     every time it fires, grab the location
     of touch and assign it to box */
     
+    box.ondragstart = iniciaDrag;
+    dest.ondragover = overDrag;
+
+    let idelement =""
+    function iniciaDrag(ev){
+      idelement =  ev.target.id;
+    }
+
+    function overDrag(ev){
+      ev.preventDefault();
+    }
+
+    function fimDrag(ev){
+      ev.preventDefault();
+      ev.target.appendChild(document.getElementById(idelement));
+    }
+
     box.addEventListener('touchmove', function(e) {
+      iniciaDrag(e);
       // grab the location of touch
       var touchLocation = e.targetTouches[0];
-      
+      console.log(box.style.width);
       // assign box new coordinates based on the touch.
-      box.style.left = (touchLocation.pageX + Math.floor(box.style.width/2)) + 'px';
-      box.style.top = touchLocation.pageY + 'px';
+      box.style.left = (touchLocation.pageX - 50) + 'px';
+      box.style.top = (touchLocation.pageY -50) + 'px';
     })
     
     /* record the position of the touch
@@ -23,6 +41,7 @@ window.onload = function() {
       // current box position.
       var x = parseInt(box.style.left);
       var y = parseInt(box.style.top);
+      if(x > dest)
     })
     
   }
